65fe47fa469221057b14686958d445f3ef150365
Tonoak2129 <96166620+Tonoak2129@users.noreply.github.com>
2026-02-10 21:22:28 +0700

    Refactor CoffeeShop ordering methods and price calculation
    Refactored orderEspresso and orderFrappuccino methods to improve functionality and added shot handling for espresso. Updated main method to calculate total price correctly.

--- CoffeeShop/CoffeeShop.java
+++ CoffeeShop/CoffeeShop.java
@@ -5,47 +5,44 @@ import java.util.Scanner;
 public class CoffeeShop {
     public static Scanner scn = new Scanner(System.in);
 
-    public static Espresso orderEspresso(){
+    public static Espresso orderEspresso() {
         System.out.print("Enter a size: ");
         String size = scn.next();
+
+        Espresso espresso = new Espresso("Espresso", 35, size);
 
         System.out.print("Press 'y' or 'Y' for adding a shot: ");
         String addShot = scn.next();
 
-        int shot = 0;
         if (addShot.equalsIgnoreCase("y")) {
             System.out.print("How many shots for adding in : ");
-            shot = scn.nextInt();
-        }
-        Espresso espresso = new Espresso("Espresso",35,size,shot);
+            int shot = scn.nextInt();
 
-        System.out.println(espresso.toString());
+            espresso.addShot(shot);
+        }
+        System.out.println(espresso);
         return espresso;
     }
-
-    public static Frappuccino orderFrappuccino(){
+    public static Frappuccino orderFrappuccino() {
         System.out.print("Enter a size: ");
         String size = scn.next();
-
 
         System.out.print("Do you would like to add whipped cream [y/Y]? ");
         String addWhip = scn.next();
 
-        boolean whipped = false;
-        if (addWhip.equalsIgnoreCase("y")) whipped = true;
+        boolean whipped = addWhip.equalsIgnoreCase("y");
 
-        Frappuccino frappuccino = new Frappuccino("Frappuccino",40,size,whipped);
+        Frappuccino frappuccino =
+                new Frappuccino("Frappuccino", 40, size, whipped);
 
-        System.out.println(frappuccino.toString());
+        System.out.println(frappuccino);
         return frappuccino;
-    }
-    public static double calculateFinalPrice() {
-        return orderEspresso().getBasePrice()+ orderFrappuccino().getBasePrice();
     }
 
     public static void main(String[] args) {
         int espresso = 0;
         int frappuccino = 0;
+        double totalPrice = 0;
         while (true) {
             System.out.println("Press 1 for ordering Espresso");
             System.out.println("Press 2 for ordering Frappuccino");
@@ -53,12 +50,14 @@ public class CoffeeShop {
 
             int option = scn.nextInt();
             if (option == 1) {
-                orderEspresso();
-                espresso += 1;
+                Espresso e = orderEspresso();
+                totalPrice += e.calculateFinalPrice();
+                espresso++;
             }
             else if (option == 2) {
-                orderFrappuccino();
-                frappuccino += 1;
+                Frappuccino f = orderFrappuccino();
+                totalPrice += f.calculateFinalPrice();
+                frappuccino++;
             }
 
             System.out.println();
@@ -67,8 +66,9 @@ public class CoffeeShop {
             if (continueOrder.equalsIgnoreCase("y")) continue;
             else break;
         }
+        System.out.println();
         System.out.println("You ordered " + espresso + " Espresso");
-        System.out.println("You ordered " + frappuccino + "Frappuccino");
-        System.out.println("Total Price = " + calculateFinalPrice());
+        System.out.println("You ordered " + frappuccino + " Frappuccino");
+        System.out.println("Total Price = " + totalPrice);
     }
 }
